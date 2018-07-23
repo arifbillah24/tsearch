26734b20533fde7e371f14ffe00c2ce5ccd74e46
arifbillah24 <arif.billah24@mail.com>
2018-07-23 20:55:56 +0530

    Update header.js

--- src/app/header.js
+++ src/app/header.js
@@ -56,53 +56,6 @@ class Header extends React.Component {
 					<CardMedia
 						overlay={<CardTitle className='header-title' title={__('Local Torrent!')} style={{paddingTop: 2}} subtitle={
 							<div>
-								<div className='row' style={{position: 'absolute', top: -65}}>
-									<Tooltip hint={__('main settings')}>
-										<svg className='clickable'
-											onClick={() => {
-												window.router('/config')
-											}}
-											fill='white' style={{height: 45, margin: 4}} viewBox="0 0 932.179 932.179">
-											<g>
-												
-											</g>
-										</svg>
-									</Tooltip>
-									<Tooltip hint={__('filters settings')}>
-										<svg className='clickable'
-											onClick={() => {
-												window.router('/filters')
-											}}
-											fill='white' style={{height: 45, margin: 4}} viewBox="0 0 512 512">
-											<g>
-												<g>
-													
-												</g>
-											</g>
-											<g>
-												<g>
-													
-												</g>
-											</g>
-											<g>
-												<g>
-													
-												</g>
-											</g>
-											<g>
-												<g>
-													
-												</g>
-											</g>
-											<g>
-												<g>
-													
-												</g>
-											</g>
-										</svg>
-									</Tooltip>
-								</div>
-
 								{__('Welcome to Local Torrent')} {__('This is file search engine based on the torrents from the internet')}. 
 								{__('Here you can easily find torrent or file that you intrested for')}. {__('We are not responsible for any content')}:
 								{__('this is only information about content that collected automatically')}!
@@ -114,69 +67,8 @@ class Header extends React.Component {
 							backgroundSize: 'cover',
 							transition: '1s'
 						}}>
-							<RaisedButton
-								label={__('Feed')}
-								onClick={() => {
-									window.router('/')
-								}}
-								backgroundColor='#69238c'
-								labelColor='white'
-								style={{height: 60, borderRadius: 6, margin: 5, zIndex: 1}}
-								buttonStyle={{borderRadius: 5}}
-								icon={<svg fill='white' style={{height: 28}} viewBox="0 0 64.051 64.051">
-									<g>
-										
-									</g>
-								</svg>
-         
-								}
-							/>
-							<RaisedButton
-								label={__('Top')}
-								onClick={() => {
-									window.router('/top')
-								}}
-								backgroundColor='#B1CE57'
-								labelColor='white'
-								style={{height: 60, width: 120, borderRadius: 5, margin: 5, zIndex: 1}}
-								buttonStyle={{borderRadius: 5}}
-								icon={<svg fill='white' style={{height: 30}} viewBox="0 0 284.929 284.929">
-									<g>
-										
-									</g>
-								</svg>
-								}
-							/>
-							<RaisedButton
-								label={__('Downloads')}
-								onClick={() => {
-									window.router('/downloads')
-								}}
-								backgroundColor='#2080E4'
-								labelColor='white'
-								style={{height: 60, borderRadius: 6, margin: 5, zIndex: 1}}
-								buttonStyle={{borderRadius: 5}}
-								icon={<svg fill='white' style={{height: 30}} viewBox="0 0 548.176 548.176">
-									
-								</svg>
-								}
-							/>
-							<RaisedButton
-								label={__('Activity')}
-								onClick={() => {
-									window.router('/activity')
-								}}
-								backgroundColor='#2a5cba'
-								labelColor='white'
-								style={{height: 60, width: 160, borderRadius: 5, margin: 5, zIndex: 1}}
-								buttonStyle={{borderRadius: 5}}
-								icon={<svg fill='white' style={{height: 30}} viewBox="0 0 352.352 352.352">
-									<g>
-										
-									</g>
-								</svg>
-								}
-							/>
+							
+							
 							<div className='fs0-85 pad0-75 column search-panel' style={{
 								marginLeft: 'auto', 
 								marginTop: '-10px',
